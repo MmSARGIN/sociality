9e5819bb11d0c113446e76dd8872e886af8f9537
MmSARGIN <melih.sargin.53@gmail.com>
2022-09-12 20:54:06 +0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import Postcard from "./component/Postcard";
+
+
+function App() {
+  return (
+    <div className="flex h-screen bg-gray-600 justify-center items-center">
+      <Postcard />
+    </div>
+  );
+}
+
+export default App;
